ab67e5022acf07f1dc8b6a763f675ec06b81855b
Graeme Stuart <gstuart@dmu.ac.uk>
2026-06-15 09:04:26 +0100

    move functions into tools module

--- js/thing.js
+++ js/thing.js
@@ -1,18 +1,7 @@
 import { mat4 } from 'https://wgpu-matrix.org/dist/3.x/wgpu-matrix.module.min.js';
+import { randomOrientation, randomTranslation } from './tools';
 
-function randomOrientation(tm) {
-    tm = mat4.rotateX(tm, 4 * Math.PI * (Math.random() - 0.5));
-    tm = mat4.rotateY(tm, 4 * Math.PI * (Math.random() - 0.5));
-    return mat4.rotateZ(tm, 4 * Math.PI * (Math.random() - 0.5));
-}
 
-function randomTranslation() {    
-    return mat4.translation([
-        (Math.random() - 0.5) * 2,
-        (Math.random() - 0.5) * 2,
-        0.25,        
-    ]);
-}
 
 export class Thing {
 
--- js/tools.js
+++ js/tools.js
@@ -8,3 +8,17 @@ export function mappedBuffer(device, data, args) {
     buffer.unmap();
     return buffer;
 }
+
+export function randomOrientation(tm) {
+    tm = mat4.rotateX(tm, 4 * Math.PI * (Math.random() - 0.5));
+    tm = mat4.rotateY(tm, 4 * Math.PI * (Math.random() - 0.5));
+    return mat4.rotateZ(tm, 4 * Math.PI * (Math.random() - 0.5));
+}
+
+export function randomTranslation() {    
+    return mat4.translation([
+        (Math.random() - 0.5) * 2,
+        (Math.random() - 0.5) * 2,
+        0.25,        
+    ]);
+}
